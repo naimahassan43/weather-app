91d5693a5ec9b7c318ce0d0faa95cb3e3902bbbd
Naima Hassan <naima.hassan434@gmail.com>
2021-08-22 00:05:55 +0100

    Api include

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 // Write your javascript code here
+//Api endpoint
+const weatherApiKey = "ab71f09d1c46c7344587c67407c9583e";
+const weatherEndPoint = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${weatherApiKey}`;
